44f881e5188f9bf00748126d568b6b26737a6489
Fademacios <122602505+Fademacios@users.noreply.github.com>
2025-02-10 11:59:27 +0100

    g

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function writeNameWithParametrs(imie, powitanie){
-  document.write(imie + powitanie)}
-
-document.write(writeNameWithParametrs('Maciek', 'Cześć'))
+function writeNameWithParametrs(imie) {
+  consle.log("cześć, " + imie + "! Miło cię widzieć.");
+}
+const imieUzytkownika = prompt("podaj swoje imie:");
+writeNameWithParametrs(imieUzytkownika);
